c64b1faa2fcd07932f0ea412280d9372a7355ab5
Benjamin Fischer <c4spar@gmx.de>
2022-02-27 19:14:39 +0100

    refactor: refactor main file

--- main.ts
+++ main.ts
@@ -1,13 +1,7 @@
-import {
-  ExamplesDataProvider,
-  serve,
-} from "https://raw.githubusercontent.com/c4spar/cliffy.io/v0.0.2/mod.tsx";
+import { serve } from "https://raw.githubusercontent.com/c4spar/night-owl/v0.1.4/mod.ts";
 
 await serve({
   repository: "c4spar/deno-cliffy",
-  src: [{ src: "src", prefix: "/docs" }],
-  providers: [{
-    component: ExamplesDataProvider,
-    props: { src: "examples", selected: "command.ts" },
-  }],
+  src: "./",
+  nav: { collapse: true },
 });
