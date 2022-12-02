e1fe3adab56800aac84363b84806af068f8433dc
Benjamin Fischer <c4spar@gmx.de>
2022-12-03 00:46:19 +0100

    chore: add deno.jsonc config and remove Makefile

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import { serve } from "https://raw.githubusercontent.com/c4spar/night-owl/v0.1.4/mod.ts";
 
 await serve({
+  name: "Cliffy",
   repository: "c4spar/deno-cliffy",
   src: "./",
   nav: { collapse: true },
